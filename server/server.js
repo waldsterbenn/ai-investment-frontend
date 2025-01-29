4f18bdbae327b35f209030a05ed8f3b43f21b052
Lars Skovbo <lars.skovbo@imotionsglobal.com>
2025-01-29 14:39:31 +0100

    Added new service endpoint

--- server/server.js
+++ server/server.js
@@ -206,3 +206,58 @@ app.post("/api/run-financial-analysis", async (req, res) => {
     }
   });
 });
+
+app.post("/api/run-advice", async (req, res) => {
+  console.debug("/api/run-advice > script caller triggered");
+  console.debug("*********");
+
+  // Path to your Python script, could be absolute
+  const pythonScriptPath = path.join(
+    __dirname,
+    "../../ai-investment-manager/single_advice.py"
+  );
+
+  // Serialize the JSON object to a string
+  // const jsonDataString = JSON.stringify(req.body, null, 0).replace(/"/g, "'");
+  // const encoded = encodeURIComponent(jsonDataString);
+  const jsonDataString = JSON.stringify(req.body);
+  const encodedData = Buffer.from(jsonDataString, "utf-8").toString("base64");
+  //console.debug(encoded);
+
+  // Spawn the Python script with the JSON data as an argument
+  const childProcess = spawn(ServerConfig.conda_exe_path, [
+    "run",
+    "-p",
+    ServerConfig.conda_env_path,
+    "python",
+    pythonScriptPath,
+    encodedData,
+  ]);
+
+  let output = "";
+  let error = "";
+
+  // Capture stdout data from the script
+  childProcess.stdout.on("data", (data) => {
+    output += data.toString();
+    console.debug("script out: " + output);
+  });
+
+  // Capture stderr data from the script
+  childProcess.stderr.on("data", (err) => {
+    error += err.toString();
+    console.debug("script err: " + error);
+  });
+
+  // Handle when the script completes
+  childProcess.on("close", (code) => {
+    if (code === 0) {
+      console.debug("script sucess");
+      const report = output.split("ADVICE:")[1];
+      res.json({ success: true, report });
+    } else {
+      console.debug("script failed", error);
+      res.status(500).json({ success: false, error });
+    }
+  });
+});
